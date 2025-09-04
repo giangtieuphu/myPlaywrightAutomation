d29ccecae72ffb7870e9088efc1124cc097fc32a
giangtieuphu <truonghong.giang191987@gmail.com>
2025-09-04 21:40:17 +0700

    added looping matching elements

---
+++ src/tests/07_loop_elements.spec.ts
@@ -0,0 +1,14 @@
+import { test } from "@playwright/test";
+
+test("Loop through elements", async ({ page }) => {
+
+    await page.goto("https://github.com/giangtieuphu");
+
+    const repolinks = page.$$('[class="repo"]');
+
+    for (const repoLink of await repolinks) {
+        console.log(await repoLink.textContent());
+    }
+
+    page.close();
+});
