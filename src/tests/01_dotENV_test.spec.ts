c6f707a255362606a6b726bb23c84e6ba8b6541a
giangtieuphu <truonghong.giang191987@gmail.com>
2025-09-04 20:29:01 +0700

    updated new email password for dotENV test

--- src/tests/01_dotENV_test.spec.ts
+++ src/tests/01_dotENV_test.spec.ts
@@ -1,26 +1,23 @@
 import {test, expect, Locator} from '@playwright/test';
 import { strict } from 'assert';
 
-test.skip('Testing different dotenv data', async ({page}) => {
+test('Testing different dotenv data', async ({page}) => {
 
     await page.goto(process.env.BASE_URL as string);
 
-    let myTitle: String = "My Account"
-
+    const myTitle = "My Account";
     const emailField:Locator = page.locator('#input-email');
     const passwordField:Locator = page.locator('#input-password');
     const loginButton:Locator = page.locator('input[value="Login"]');
 
-    await page.locator('#input-email').fill(process.env.EMAIL as string);
-    await page.locator('#input-password').fill(process.env.PASSWORD as string);
+    await page.locator('#input-email').fill(process.env.MYEMAIL as string);
+    await page.locator('#input-password').fill(process.env.MYPASSWORD as string);
     await page.locator('input[value="Login"]').click();
 
-    //expect(page).toHaveURL('https://naveenautomationlabs.com/opencart/index.php?route=account/account');
-    
-    //expect(strict.equal(await page.title(), myTitle));
-    
-    expect(page).toHaveTitle(myTitle as string);
+    await expect(page).toHaveURL('https://naveenautomationlabs.com/opencart/index.php?route=account/account');
 
-    await page.screenshot({path: 'screenshot.png'});
+    await expect(page).toHaveTitle(myTitle);
+
+    await page.screenshot({path: 'screenshots/naveenautomationlabs.png'});
     await page.close()
 });
