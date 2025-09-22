26aaa92cde35bbc42e023caae13fe3fa11c93d05
giangtieuphu <truonghong.giang191987@gmail.com>
2025-09-22 16:22:46 +0700

    try page.locator(iframe).contentFrame() and it works

--- src/tests/03_jqueryui_frameLocator.spec.ts
+++ src/tests/03_jqueryui_frameLocator.spec.ts
@@ -3,15 +3,20 @@ import { expect, test } from '@playwright/test';
 test('jQueryUI SelectMenu Test', async ({ page }) => {
     await page.goto('https://jqueryui.com/selectmenu/');
     
-    // const frameLocator = page.locator('//iframe[@class="demo-frame"]');
+    const frameLocator = page.locator('//iframe[@class="demo-frame"]')
     // const frame = page.frameLocator('//iframe[@class="demo-frame"]');
     // const frame = page.frameLocator('[class="demo-frame"]');
     const frame = page.frameLocator('iframe.demo-frame');
 
-    const speedMenu = frame.locator('#speed-button');
-    const fileMenu = frame.locator('#files-button');
-    const numberMenu = frame.locator('#number-button');
-    const titleMenu = frame.locator('#salutation-button');
+    // const speedMenu = frame.locator('#speed-button');
+    // const fileMenu = frame.locator('#files-button');
+    // const numberMenu = frame.locator('#number-button');
+    // const titleMenu = frame.locator('#salutation-button');
+
+    const speedMenu = frameLocator.contentFrame().locator('#speed-button');
+    const fileMenu = frameLocator.contentFrame().locator('#files-button');
+    const numberMenu = frameLocator.contentFrame().locator('#number-button');
+    const titleMenu = frameLocator.contentFrame().locator('#salutation-button');
 
     await test.step('Verify the presence of all the select menus', async () => {
         await expect(speedMenu).toBeVisible();
