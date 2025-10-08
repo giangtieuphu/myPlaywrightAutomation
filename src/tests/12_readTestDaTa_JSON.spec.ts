561727c3dba394223017ea241b1a92902756417d
giangtieuphu <truonghong.giang191987@gmail.com>
2025-10-08 16:44:53 +0700

    Updated page close for data driven test

--- src/tests/12_readTestDaTa_JSON.spec.ts
+++ src/tests/12_readTestDaTa_JSON.spec.ts
@@ -1,10 +1,9 @@
 import {test, expect} from '@playwright/test' 
 import users from '../data/vercel.json'
 
-// test.describe('Data Driven Sign-Up Test', () => {
-    console.log("Number of users to sign-up: ", users.length)
-    for (const user of users) { 
-        test(`Sign-up for user: ${user.username}`, async ({page})=>{
+test.describe('Data Driven Sign-Up Test', () => {
+    for (const user of users) {
+        test(`Sign-up for user: ${user.username}`, async ({ page }) => {
             await page.goto('https://freelance-learn-automation.vercel.app/signup')
             await page.waitForLoadState("networkidle")
             await page.getByRole('textbox', { name: 'Name' }).fill(user.fullname)
@@ -20,15 +19,17 @@ import users from '../data/vercel.json'
             await page.getByRole('button', { name: 'Sign up' }).click()
             await page.waitForLoadState("networkidle")       
 
-            expect(page.getByRole('heading', { name: 'error Email already' })).toBeVisible()
+            expect(page.getByRole('heading', { name: 'Email already registered!' })).toBeVisible()
+            
+            await page.waitForTimeout(1000)
+            await page.close()
         })
     }
-// })
+})
 
-// test.describe('Data Driven Login-Logout Test', () => {
-    console.log("Number of users to LoginLogout: ", users.length)
-    for (const user of users) {   
-        test(`Login-Logout as: ${user.username}`, async ({page}) => {
+test.describe('Data Driven Login-Logout Test', () => {
+    for (const user of users) {
+        test(`Login-Logout as: ${user.username}`, async ({ page }) => {
             await page.goto('https://freelance-learn-automation.vercel.app/login')
             await page.waitForLoadState("networkidle")
 
@@ -37,13 +38,16 @@ import users from '../data/vercel.json'
             await page.getByRole('button', { name: 'Sign in' }).click()
             await page.waitForLoadState('networkidle');
             
-            expect(page).toHaveURL('https://freelance-learn-automation.vercel.app/')
+            expect(page).toHaveURL('https://freelance-learn-automation.vercel.app')
 
             await page.getByRole('img', { name: 'menu' }).click()
             await page.getByRole('button', { name: 'Sign out' }).click()
             await page.waitForLoadState('networkidle');
             
             expect(page.locator('form[class="login-form"]')).toBeVisible()
+            
+            await page.waitForTimeout(1000)
+            await page.close()
         })
     }
-// })
+})
